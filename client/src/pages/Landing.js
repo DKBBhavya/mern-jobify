3c8b6bccdee8243bcd1a8ea175396a0684440940
Bhavya Agarwal <bhavya.dkb@gmail.com>
2023-05-07 17:19:13 +0530

    Completed Landing Page

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,31 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/Testing";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            convallis lacinia tortor ac vestibulum. Donec egestas augue orci, ac
+            maximus diam cursus vel. Cras nec quam in libero finibus cursus sed
+            id odio. Nullam lobortis rutrum erat, eu convallis nunc. Donec
+            dictum luctus ex ut maximus.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
