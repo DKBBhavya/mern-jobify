390bcded082c50a3563a6d94edae39e743cfc5a0
Bhavya Agarwal <bhavya.dkb@gmail.com>
2023-05-08 19:58:06 +0530

    Added Routes

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const app = express();
 import dotenv from "dotenv";
 import connectDB from "./db/connect.js";
 dotenv.config();
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  throw new Error("error");
   res.send("Welcome!");
 });
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
