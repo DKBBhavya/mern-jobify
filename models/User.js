1017ef14988ed88580898e51d7f8e6e8126284ca
Bhavya Agarwal <bhavya.dkb@gmail.com>
2023-05-11 00:51:06 +0530

    Hashed Passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -25,6 +26,11 @@ const UserSchema = new mongoose.Schema({
   },
   lastName: { type: String, maxlength: 20, trim: true, default: "lastName" },
   location: { type: String, maxlength: 20, trim: true, default: "my city" },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.model("User", UserSchema);
