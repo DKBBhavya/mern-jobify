b89c991d0035a0dd42e72024f377dd94fa930ba1
Bhavya Agarwal <bhavya.dkb@gmail.com>
2023-05-27 13:21:43 +0530

    Implemented Monthly Applications

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,4 +1,21 @@
+import { useState } from "react";
+import Wrapper from "../assets/wrappers/ChartsContainer";
+import { useAppContext } from "../context/appContext";
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
+
 const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+
+  return (
+    <Wrapper>
+      <h4> monthly applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
 export default ChartsContainer;
